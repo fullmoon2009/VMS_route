3753e9977b671ecd3948f7d0370ccb4b2bd331b9
fullmoon2009 <160104012+fullmoon2009@users.noreply.github.com>
2026-02-04 06:43:50 +0000

    Update code section

--- VMSRoute.user.js
+++ VMSRoute.user.js
@@ -760,21 +760,7 @@
 
 	window.__VMS_REUSE_CACHE__ = reuseCache;
 })();
-// ==UserScript==
-// @name         New Userscript
-// @namespace    http://tampermonkey.net/
-// @version      2026-01-23
-// @description  try to take over the world!
-// @author       You
-// @match        https://*/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
 
-(function () {
-	'use strict';
 
-	// Your code here...
-})();
 
 
